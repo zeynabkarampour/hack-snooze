cad864fb427ea74e0f556d0bd45e79543ed69e30
zeynabkarampour <maryamshokoohhi@gmail.com>
2020-10-02 11:32:02 -0700

    first commit

---
+++ js/nav.js
@@ -0,0 +1,41 @@
+/******************************************************************************
+ * Handling navbar clicks and updating navbar
+ */
+
+/** Show main list of all stories when click site name */
+
+function navAllStories(evt) {
+  console.debug("navAllStories", evt);
+  hidePageComponents();
+  putStoriesOnPage();
+}
+
+$body.on("click", "#nav-all", navAllStories);
+
+/** Show login/signup on click on "login" */
+
+function navLoginClick(evt) {
+  console.debug("navLoginClick", evt);
+  hidePageComponents();
+  $loginForm.show();
+  $signupForm.show();
+}
+
+$navLogin.on("click", navLoginClick);
+
+/** When a user first logins in, update the navbar to reflect that. */
+
+function updateNavOnLogin() {
+  console.debug("updateNavOnLogin");
+  $(".main-nav-links").show();
+  $navLogin.hide();
+  $navLogOut.show();
+  $loginForm.hide();
+  $signupForm.hide();
+  $navUserProfile.text(`${currentUser.username}`).show();
+}
+
+/** When a user click on Submit link in Navbar, the submit form will show on page. */
+$submitlink.on("click" , function(){
+  $submitNewStoryForm.show();
+})
---
+++ js/stories.js
@@ -0,0 +1,77 @@
+// This is the global list of the stories, an instance of StoryList
+let storyList;
+let $storySubmitForm = $("#submit-form");
+
+/** Get and show stories when site first loads. */
+
+async function getAndShowStoriesOnStart() {
+  storyList = await StoryList.getStories();
+  $storiesLoadingMsg.remove();
+
+  putStoriesOnPage();
+}
+
+/**
+ * A render method to render HTML for an individual Story instance
+ * - story: an instance of Story
+ *
+ * Returns the markup for the story.
+ */
+
+function generateStoryMarkup(story) {
+  // console.debug("generateStoryMarkup", story);
+
+  const hostName = story.getHostName();
+  return $(`
+      <li id="${story.storyId}">
+        <a href="${story.url}" target="a_blank" class="story-link">
+          ${story.title}
+        </a>
+        <small class="story-hostname">(${hostName})</small>
+        <small class="story-author">by ${story.author}</small>
+        <small class="story-user">posted by ${story.username}</small>
+      </li>
+    `);
+}
+
+/** Gets list of stories from server, generates their HTML, and puts on page. */
+
+function putStoriesOnPage() {
+  console.debug("putStoriesOnPage");
+
+  $allStoriesList.empty();
+
+  // loop through all of our stories and generate HTML for them
+  for (let story of storyList.stories) {
+    const $story = generateStoryMarkup(story);
+    $allStoriesList.append($story);
+  }
+
+  $allStoriesList.show();
+}
+
+$storySubmitForm.on("submit" , async function (evt){
+  evt.preventDefault();
+  let createAuthor = $(evt.target).find("#create-author").val();
+  let createTitle = $(evt.target).find("#create-title").val();
+  let createUrl =$(evt.target).find("#create-url").val();
+  let newStory = await storyList.addStory(currentUser,{title : createTitle, author : createAuthor, url: createUrl});
+  const $story = generateStoryMarkup(newStory);
+  $allStoriesList.prepend($story);
+})
+
+
+
+// /** Sync new story information to localStorage.
+//  *
+//  * We store the author/title/url in localStorage , and them show new story on page
+//  */
+
+// function saveNewStoryInLocalStorage() {
+//   console.debug("saveNewStoryInLocalStorage");
+//   if (currentUser) {
+//     localStorage.setItem("token", currentUser.loginToken);
+//     localStorage.setItem("username", currentUser.username);
+//     localStorage.setItem("username", currentUser.username);
+//   }
+// }
